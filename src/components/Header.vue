f3281617aa4fbdfe141c0e9d06bf527a415823a1
julien <julien.clairo@iia-formation.fr>
2023-07-13 23:50:37 +0200

    Initialisation du projet Portfolio JS Mise en place du router Création d'un Header Component

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+      <ul>
+          <li>
+              <RouterLink to="/">Parcours scolaire</RouterLink>
+          </li>
+          <li>
+              <RouterLink to="/">Parcours professionnel</RouterLink>
+          </li>
+          <li>
+              <RouterLink to="/">Compétences</RouterLink>
+          </li>
+          <li>
+              <RouterLink to="/">Portfolio</RouterLink>
+          </li>
+      </ul>
+  </div>
+    <RouterView />
+</template>
+
+<style scoped>
+
+  a{
+      text-decoration: none;
+      color: black;
+  }
+
+  div{
+      height: 300px;
+      width: 100%;
+      background-color: coral;
+  }
+  ul{
+      display: flex;
+      gap : 2rem;
+      justify-content: center;
+      list-style: none;
+  }
+
+</style>
